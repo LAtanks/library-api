9cc2dafe7d37a08816c98e554b39bb67b7eebd48
Giovanni Samuel <76797064+LAtanks@users.noreply.github.com>
2026-03-31 22:07:19 -0300

    add spring starter & devtools & validation depedencia

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,9 +20,17 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+
     compileOnly("org.projectlombok:lombok")
+
     runtimeOnly("com.mysql:mysql-connector-j")
+
     annotationProcessor("org.projectlombok:lombok")
+
+    
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testCompileOnly("org.projectlombok:lombok")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
